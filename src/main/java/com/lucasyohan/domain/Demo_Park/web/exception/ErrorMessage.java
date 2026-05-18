a85c4348b82aedf4344744cacd91f10b4fab62d1
Lucas Yohan <lucalen.y@live.com>
2026-05-18 16:31:30 -0300

    Adicionado parametro JsonInclude

--- src/main/java/com/lucasyohan/domain/Demo_Park/web/exception/ErrorMessage.java
+++ src/main/java/com/lucasyohan/domain/Demo_Park/web/exception/ErrorMessage.java
@@ -1,6 +1,7 @@
 package com.lucasyohan.domain.Demo_Park.web.exception;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.Getter;
 import lombok.ToString;
@@ -20,6 +21,7 @@ public class ErrorMessage {
     private int status;
     private String statusText;
     private String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private Map<String, String> errors = new HashMap<>();
 
     public ErrorMessage() {

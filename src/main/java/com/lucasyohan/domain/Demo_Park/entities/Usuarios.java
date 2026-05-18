9e9b9ef22a39e4a59afc969a6ba86f695bd2fea7
Lucas Yohan <lucalen.y@live.com>
2026-05-18 16:30:29 -0300

    Adicionado strategy id

--- src/main/java/com/lucasyohan/domain/Demo_Park/entities/Usuarios.java
+++ src/main/java/com/lucasyohan/domain/Demo_Park/entities/Usuarios.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 public class Usuarios implements Serializable {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "username", nullable = false, unique = true, length = 100)
     private String username;

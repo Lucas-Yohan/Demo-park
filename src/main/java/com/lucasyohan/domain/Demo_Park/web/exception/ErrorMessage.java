7e3cb44a03fc3330f47a5f331d25e9a8891f3edc
Lucas Yohan <lucalen.y@live.com>
2026-05-18 15:15:53 -0300

    Refatorado classe do errorMessage

--- src/main/java/com/lucasyohan/domain/Demo_Park/web/exception/ErrorMessage.java
+++ src/main/java/com/lucasyohan/domain/Demo_Park/web/exception/ErrorMessage.java
@@ -1,24 +1,52 @@
 package com.lucasyohan.domain.Demo_Park.web.exception;
 
 
-import java.util.HashMap;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@ToString
 public class ErrorMessage {
 
-    private String message;
     private String path;
     private String method;
     private int status;
-    private HashMap<String, String> errors;
+    private String statusText;
+    private String message;
+    private Map<String, String> errors = new HashMap<>();
 
     public ErrorMessage() {
     }
 
-    public ErrorMessage(String message, String path, String method, int status) {
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message) {
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
         this.message = message;
-        this.path = path;
-        this.method = method;
-        this.status = status;
+    }
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message, BindingResult result) {
+        this.path = request.getRequestURI();
+        this.method = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
+        this.message = message;
+        addErrors(result);
+    }
+
+    private void addErrors(BindingResult result) {
+        this.errors = new HashMap<>();
+        for (FieldError error : result.getFieldErrors()) {
+            this.errors.put(error.getField(), error.getDefaultMessage());
+        }
     }
 
 

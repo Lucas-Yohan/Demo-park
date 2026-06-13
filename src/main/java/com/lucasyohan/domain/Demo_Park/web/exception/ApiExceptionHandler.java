9f862adb019cccd3070e6e0fb869ffa724f07642
Lucas Yohan <lucalen.y@live.com>
2026-06-13 01:11:27 -0300

    Correcao classe para tratamento de erro.

--- src/main/java/com/lucasyohan/domain/Demo_Park/web/exception/ApiExceptionHandler.java
+++ src/main/java/com/lucasyohan/domain/Demo_Park/web/exception/ApiExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.lucasyohan.domain.Demo_Park.web.exception;
 
 import com.lucasyohan.domain.Demo_Park.exceptions.EntityNotFoundException;
-import com.lucasyohan.domain.Demo_Park.exceptions.MethodArgumentNotValidException;
 import com.lucasyohan.domain.Demo_Park.exceptions.UsernameUniqueViolationException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 

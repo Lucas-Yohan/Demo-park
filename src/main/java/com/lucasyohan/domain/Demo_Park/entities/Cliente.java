11fb6cb3c1b113f11a3f21c61829292e6da5bd8f
Lucas Yohan <lucalen.y@live.com>
2026-06-17 22:38:41 -0300

    Inclusao Cliente entity

---
+++ src/main/java/com/lucasyohan/domain/Demo_Park/entities/Cliente.java
@@ -0,0 +1,73 @@
+package com.lucasyohan.domain.Demo_Park.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", unique = true, nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "cpf", unique = true, nullable = false, length = 11)
+    private String cpf;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario usuario;
+
+    @CreatedDate
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+    @LastModifiedDate
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+    @CreatedBy
+    @Column(name = "criado_por")
+    private LocalDateTime criadoPor;
+    @LastModifiedBy
+    @Column(name = "modificado_por")
+    private LocalDateTime modificadoPor;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                '}';
+    }
+
+}

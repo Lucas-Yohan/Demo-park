85848cbbb1b65c68b27e37b02b0e390ae399ebf1
Lucas Yohan <lucalen.y@live.com>
2026-06-17 22:41:57 -0300

    Inclusao securityScheme

--- src/main/java/com/lucasyohan/domain/Demo_Park/config/SpringOpenApiConfig.java
+++ src/main/java/com/lucasyohan/domain/Demo_Park/config/SpringOpenApiConfig.java
@@ -1,11 +1,14 @@
 package com.lucasyohan.domain.Demo_Park.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class SpringOpenApiConfig {
@@ -13,6 +16,8 @@ public class SpringOpenApiConfig {
     @Bean
         public OpenAPI openAPI() {
         return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("security", securityScheme()))
                 .info(new Info()
                         .title("Demo Park API")
                         .version("1.0")
@@ -23,6 +28,16 @@ public class SpringOpenApiConfig {
                                 .name("Lucas Yohan")
                                 .email("examplemail.com"))
                         .description("API para gerenciamento do estacionamento do Demo Park"));
+    }
+
+    private SecurityScheme securityScheme() {
+        return new SecurityScheme()
+                .description("JWT Authentication")
+                .type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .name("Security");
     }
 
 }

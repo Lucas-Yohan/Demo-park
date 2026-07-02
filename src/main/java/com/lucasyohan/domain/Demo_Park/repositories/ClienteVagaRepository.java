d5c8c6306f838b53bcb20455c1999419b9926901
Lucas Yohan <lucalen.y@live.com>
2026-07-01 23:51:46 -0300

    feat(repo): adicionado repo VagaRepository e ClienteVagaRepository

---
+++ src/main/java/com/lucasyohan/domain/Demo_Park/repositories/ClienteVagaRepository.java
@@ -0,0 +1,11 @@
+package com.lucasyohan.domain.Demo_Park.repositories;
+
+import com.lucasyohan.domain.Demo_Park.entities.ClienteVaga;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClienteVagaRepository extends JpaRepository<ClienteVaga, Long> {
+
+    Optional<ClienteVaga> findByReciboAndDataSaidaIsNull(String recibo);
+}

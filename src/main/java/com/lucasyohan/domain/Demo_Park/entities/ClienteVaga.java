e1127f66aaba73b265d1e90ac3fef44af3bd193d
Lucas Yohan <lucalen.y@live.com>
2026-07-01 23:49:32 -0300

    feat(entity): adicionados entidades VAGA e ClienteVaga

---
+++ src/main/java/com/lucasyohan/domain/Demo_Park/entities/ClienteVaga.java
@@ -0,0 +1,81 @@
+package com.lucasyohan.domain.Demo_Park.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "cliente_tem_vagas")
+@EntityListeners(AuditingEntityListener.class)
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClienteVaga implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "recibo", nullable = false, unique = true, length = 15)
+    private String recibo;
+
+    @Column(name = "placa", nullable = false, length = 8)
+    private String placa;
+    @Column(name = "marca", nullable = false, length = 50)
+    private String marca;
+    @Column(name = "modelo", nullable = false, length = 50)
+    private String modelo;
+    @Column(name = "cor", nullable = false, length = 50)
+    private String cor;
+    @Column(name = "data_entrada", nullable = false)
+    private LocalDateTime dataEntrada;
+    @Column(name = "data_saida")
+    private LocalDateTime dataSaida;
+    @Column(name = "valor", columnDefinition = "decimal(7,2)")
+    private BigDecimal valor;
+    @Column(name = "desconto", columnDefinition = "decimal(7,2)")
+    private BigDecimal desconto;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_cliente", nullable = false)
+    private Cliente cliente;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_vaga", nullable = false)
+    private Vaga vaga;
+
+
+    @CreatedDate
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+    @LastModifiedDate
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+    @CreatedBy
+    @Column(name = "criado_por")
+    private LocalDateTime criadoPor;
+    @LastModifiedBy
+    @Column(name = "modificado_por")
+    private LocalDateTime modificadoPor;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ClienteVaga that = (ClienteVaga) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/lucasyohan/domain/Demo_Park/entities/Vaga.java
@@ -0,0 +1,68 @@
+package com.lucasyohan.domain.Demo_Park.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "vagas")
+public class Vaga implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "codigo", nullable = false, length = 4, unique = true)
+    private String codigo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private StatusVaga status;
+
+    @CreatedDate
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+    @LastModifiedDate
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+    @CreatedBy
+    @Column(name = "criado_por")
+    private String criadoPor;
+    @LastModifiedBy
+    @Column(name = "modificado_por")
+    private String modificadoPor;
+
+    public enum StatusVaga {
+        LIVRE, OCUPADA
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Vaga vaga = (Vaga) o;
+        return Objects.equals(id, vaga.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+
+}

2041728bb7564c431a1203e9857abf22e703aa1a
Lucas Yohan <lucalen.y@live.com>
2026-07-04 21:14:15 -0300

    test: Teste do controller Estacionamento

--- src/test/java/com/lucasyohan/domain/Demo_Park/EstacionamentoIT.java
+++ src/test/java/com/lucasyohan/domain/Demo_Park/EstacionamentoIT.java
@@ -1,8 +1,7 @@
 package com.lucasyohan.domain.Demo_Park;
 
 import com.lucasyohan.domain.Demo_Park.web.dto.EstacionamentoCreateDto;
-import com.lucasyohan.domain.Demo_Park.web.dto.EstacionamentoResponseDto;
-import com.lucasyohan.domain.Demo_Park.web.exception.ErrorMessage;
+import com.lucasyohan.domain.Demo_Park.web.dto.PageableDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -167,8 +166,150 @@ public class EstacionamentoIT {
                 .expectStatus().isNotFound()
                 .expectBody()
                 .jsonPath("status").isEqualTo(404)
-                .jsonPath("path").isEqualTo("/api/v2/estacionamentos/check-in/20230313-101999")
+                .jsonPath("path").isEqualTo("/api/v2/estacionamentos/check-in/20260313-101999")
                 .jsonPath("method").isEqualTo("GET");
     }
+
+    @Test
+    public void criarCheckout_ConReciboExistente_RetornarStatus200() {
+        //'FIT-1020', 'FIAT', 'PALIO', 'VERDE', '2023-03-13 10:15:00'
+        webTestClient
+                .put()
+                .uri("/api/v2/estacionamentos/check-out/{recibo}", "20230313-101300")
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "ana@email.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("placa").isEqualTo("FIT-1020")
+                .jsonPath("marca").isEqualTo("FIAT")
+                .jsonPath("modelo").isEqualTo("PALIO")
+                .jsonPath("cor").isEqualTo("VERDE")
+                .jsonPath("recibo").exists()
+                .jsonPath("dataEntrada").isEqualTo("2026-03-13 10:15:00")
+                .jsonPath("dataSaida").exists()
+                .jsonPath("clienteCpf").isEqualTo("98401203015")
+                .jsonPath("valor").exists()
+                .jsonPath("desconto").exists()
+                .jsonPath("vagaCodigo").isEqualTo("A-01");
+
+
+    }
+
+    @Test
+    public void criarCheckout_ConReciboInexistente_RetornarErrorMessage404() {
+        //'FIT-1020', 'FIAT', 'PALIO', 'VERDE', '2023-03-13 10:15:00'
+        webTestClient
+                .put()
+                .uri("/api/v2/estacionamentos/check-out/{recibo}", "20260313-101999")
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "ana@email.com", "123456"))
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("status").isEqualTo(404)
+                .jsonPath("path").isEqualTo("/api/v2/estacionamentos/check-out/20260313-101999")
+                .jsonPath("method").isEqualTo("PUT");
+
+
+    }
+
+    @Test
+    public void criarCheckout_ComRoleCliente_RetornarErrorMessage403() {
+        //'FIT-1020', 'FIAT', 'PALIO', 'VERDE', '2023-03-13 10:15:00'
+        webTestClient
+                .put()
+                .uri("/api/v2/estacionamentos/check-out/{recibo}", "20260313-101300")
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "bia@email.com", "123456"))
+                .exchange()
+                .expectStatus().isForbidden()
+                .expectBody()
+                .jsonPath("status").isEqualTo(403)
+                .jsonPath("path").isEqualTo("/api/v2/estacionamentos/check-out/20260313-101300")
+                .jsonPath("method").isEqualTo("PUT");
+
+
+    }
+
+    @Test
+    public void buscarEstacionamentos_PorClienteCpf_RetornarStatus200() {
+        PageableDto response = webTestClient
+                .get()
+                .uri("/api/v2/estacionamentos/cpf/{cpf}?size=1&page=0", "98401203015")
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "ana@email.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(PageableDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(response).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(response.getContent()).size().isEqualTo(1);
+        org.assertj.core.api.Assertions.assertThat(response.getTotalPages()).isEqualTo(2);
+        org.assertj.core.api.Assertions.assertThat(response.getNumber()).isEqualTo(0);
+        org.assertj.core.api.Assertions.assertThat(response.getSize()).isEqualTo(1);
+
+        response = webTestClient
+                .get()
+                .uri("/api/v2/estacionamentos/cpf/{cpf}?size=1&page=1", "98401203015")
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "ana@email.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(PageableDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(response).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(response.getContent()).size().isEqualTo(1);
+        org.assertj.core.api.Assertions.assertThat(response.getTotalPages()).isEqualTo(2);
+        org.assertj.core.api.Assertions.assertThat(response.getNumber()).isEqualTo(1);
+        org.assertj.core.api.Assertions.assertThat(response.getSize()).isEqualTo(1);
+
+    }
+
+    @Test
+    public void buscarEstacionamentos_PorClienteCpfComRoleCliente_RetornarErrorMessage403() {
+        //'FIT-1020', 'FIAT', 'PALIO', 'VERDE', '2023-03-13 10:15:00'
+        webTestClient
+                .get()
+                .uri("/api/v2/estacionamentos/cpf/{cpf}?size=1&page=0", "98401203015")
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "bia@email.com", "123456"))
+                .exchange()
+                .expectStatus().isForbidden()
+                .expectBody()
+                .jsonPath("status").isEqualTo(403)
+                .jsonPath("path").isEqualTo("/api/v2/estacionamentos/cpf/98401203015")
+                .jsonPath("method").isEqualTo("GET");
+
+    }
+
+    @Test
+    public void buscarEstacionamentos_PorClienteIdLogado_RetornarStatus200() {
+        PageableDto response = webTestClient
+                .get()
+                .uri("/api/v2/estacionamentos?size=1&page=1")
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "bob@email.com", "123456"))
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(PageableDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(response).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(response.getContent()).size().isEqualTo(1);
+        org.assertj.core.api.Assertions.assertThat(response.getTotalPages()).isEqualTo(2);
+        org.assertj.core.api.Assertions.assertThat(response.getNumber()).isEqualTo(1);
+        org.assertj.core.api.Assertions.assertThat(response.getSize()).isEqualTo(1);
+    }
+
+    @Test
+    public void buscarEstacionamentos_PorClienteIdLogadoNaoAutorizado_RetornarErroMessage403() {
+        webTestClient
+                .get()
+                .uri("/api/v2/estacionamentos?size=1&page=1")
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "ana@email.com", "123456"))
+                .exchange()
+                .expectStatus().isForbidden()
+                .expectBody()
+                .jsonPath("status").isEqualTo(403)
+                .jsonPath("path").isEqualTo("/api/v2/estacionamentos")
+                .jsonPath("method").isEqualTo("GET");
+    }
+
 }
 

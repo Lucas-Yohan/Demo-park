469aa3c61df78379fdaed6a461ade10b4e843c6f
Lucas Yohan <lucalen.y@live.com>
2026-07-04 21:07:50 -0300

    feat(service): adicionados regra para buscar vagas por cpf, usuario id, retornar vezes estacionamentos.

--- src/main/java/com/lucasyohan/domain/Demo_Park/services/ClienteVagaService.java
+++ src/main/java/com/lucasyohan/domain/Demo_Park/services/ClienteVagaService.java
@@ -3,7 +3,11 @@ package com.lucasyohan.domain.Demo_Park.services;
 import com.lucasyohan.domain.Demo_Park.entities.ClienteVaga;
 import com.lucasyohan.domain.Demo_Park.exceptions.EntityNotFoundException;
 import com.lucasyohan.domain.Demo_Park.repositories.ClienteVagaRepository;
+import com.lucasyohan.domain.Demo_Park.repositories.projection.ClienteVagaProjection;
+import com.lucasyohan.domain.Demo_Park.web.dto.PageableDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,5 +28,20 @@ public class ClienteVagaService {
         return clienteVagaRepository.findByReciboAndDataSaidaIsNull(recibo).orElseThrow(
                 () -> new EntityNotFoundException(String.format("Cliente com recibo %s não encontrado ou já saiu do estacionamento.", recibo))
         );
+    }
+
+    @Transactional(readOnly = true)
+    public long getTotalDeVezesEstacionamentoCompleto(String cpf) {
+        return clienteVagaRepository.countByClienteCpfAndDataSaidaIsNotNull(cpf);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClienteVagaProjection> buscarTodosPorClienteCpf(String cpf, Pageable pageable) {
+        return clienteVagaRepository.findAllByClienteCpf(cpf, pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClienteVagaProjection> buscarTodosPorUsuarioId(Long id, Pageable pageable) {
+        return clienteVagaRepository.findAllByClienteUsuarioId(id, pageable);
     }
 }
